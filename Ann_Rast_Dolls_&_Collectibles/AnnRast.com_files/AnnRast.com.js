798fae0ed39a6788a9ee3bf40065786097703445
Winston Rast <winston+annrast@rast.org>
2025-02-28 20:58:59 -0700

    A files

---
+++ Ann_Rast_Dolls_&_Collectibles/AnnRast.com_files/AnnRast.com.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(-7.6085,2.4721),color:'#000000',opacity:0.750000}),stroke_0:new IWPhotoFrame([IWCreateImage('AnnRast.com_files/Formal_inset_01.png'),IWCreateImage('AnnRast.com_files/Formal_inset_02.png'),IWCreateImage('AnnRast.com_files/Formal_inset_03.png'),IWCreateImage('AnnRast.com_files/Formal_inset_06.png'),IWCreateImage('AnnRast.com_files/Formal_inset_09.png'),IWCreateImage('AnnRast.com_files/Formal_inset_08.png'),IWCreateImage('AnnRast.com_files/Formal_inset_07.png'),IWCreateImage('AnnRast.com_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000),shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(-6.9904,-0.3664),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('AnnRast.com_files/AnnRast.comMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();fixAllIEPNGs('Media/transparent.gif');IMpreload('AnnRast.com_files','shapeimage_2','0');IMpreload('AnnRast.com_files','shapeimage_2','1');IMpreload('AnnRast.com_files','shapeimage_2','2');Widget.onload();fixupAllIEPNGBGs();IMpreload('AnnRast.com_files','shapeimage_6','0');IMpreload('AnnRast.com_files','shapeimage_7','0');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
