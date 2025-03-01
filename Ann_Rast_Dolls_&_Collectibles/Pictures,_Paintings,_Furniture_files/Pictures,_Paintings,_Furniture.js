e5a637e4adf8255abc5071de72a7c91a7603e9c1
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:25:19 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/Pictures,_Paintings,_Furniture_files/Pictures,_Paintings,_Furniture.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Pictures,_Paintings,_Furniture_files/Formal_inset_01.png'),IWCreateImage('Pictures,_Paintings,_Furniture_files/Formal_inset_02.png'),IWCreateImage('Pictures,_Paintings,_Furniture_files/Formal_inset_03.png'),IWCreateImage('Pictures,_Paintings,_Furniture_files/Formal_inset_06.png'),IWCreateImage('Pictures,_Paintings,_Furniture_files/Formal_inset_09.png'),IWCreateImage('Pictures,_Paintings,_Furniture_files/Formal_inset_08.png'),IWCreateImage('Pictures,_Paintings,_Furniture_files/Formal_inset_07.png'),IWCreateImage('Pictures,_Paintings,_Furniture_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Pictures,_Paintings,_Furniture_files/Pictures,_Paintings,_FurnitureMoz.css')
+fixAllIEPNGs('Media/transparent.gif');applyEffects()}
