2705a8031fb4a7b9a99b823837630c30a1ae789d
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:22:34 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/Do_Not_Copy_Text_Box_files/Do_Not_Copy_Text_Box.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Do_Not_Copy_Text_Box_files/Do_Not_Copy_Text_BoxMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
