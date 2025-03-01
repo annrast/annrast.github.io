d4b735407d42e3c02df12b0ea6af8bc02ebce0fd
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:22:37 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/Doll_Dresses_&_Hats_Antique_files/Doll_Dresses_&_Hats_Antique.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,1113),url:'Doll_Dresses_%26_Hats_Antique_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Doll_Dresses_%26_Hats_Antique_files/stroke_1.png'},{rect:new IWRect(2,-2,869,4),url:'Doll_Dresses_%26_Hats_Antique_files/stroke_2.png'},{rect:new IWRect(871,-2,4,4),url:'Doll_Dresses_%26_Hats_Antique_files/stroke_3.png'},{rect:new IWRect(871,2,4,1113),url:'Doll_Dresses_%26_Hats_Antique_files/stroke_4.png'},{rect:new IWRect(871,1115,4,5),url:'Doll_Dresses_%26_Hats_Antique_files/stroke_5.png'},{rect:new IWRect(2,1115,869,5),url:'Doll_Dresses_%26_Hats_Antique_files/stroke_6.png'},{rect:new IWRect(-2,1115,4,5),url:'Doll_Dresses_%26_Hats_Antique_files/stroke_7.png'}],new IWSize(873,1117))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Doll_Dresses_&_Hats_Antique_files/Doll_Dresses_&_Hats_AntiqueMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id5');applyEffects()}
