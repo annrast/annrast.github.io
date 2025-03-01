91b25d696cbdcd6ad859f4226ebbf688ffe95f9e
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:05:34 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/TRAVEL_files/TRAVEL.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWPhotoFrame([IWCreateImage('TRAVEL_files/Formal_inset_01.png'),IWCreateImage('TRAVEL_files/Formal_inset_02.png'),IWCreateImage('TRAVEL_files/Formal_inset_03.png'),IWCreateImage('TRAVEL_files/Formal_inset_06.png'),IWCreateImage('TRAVEL_files/Formal_inset_09.png'),IWCreateImage('TRAVEL_files/Formal_inset_08.png'),IWCreateImage('TRAVEL_files/Formal_inset_07.png'),IWCreateImage('TRAVEL_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000),stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,221),url:'TRAVEL_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'TRAVEL_files/stroke_1.png'},{rect:new IWRect(2,-2,164,4),url:'TRAVEL_files/stroke_2.png'},{rect:new IWRect(166,-2,5,4),url:'TRAVEL_files/stroke_3.png'},{rect:new IWRect(166,2,5,221),url:'TRAVEL_files/stroke_4.png'},{rect:new IWRect(166,223,5,4),url:'TRAVEL_files/stroke_5.png'},{rect:new IWRect(2,223,164,4),url:'TRAVEL_files/stroke_6.png'},{rect:new IWRect(-2,223,4,4),url:'TRAVEL_files/stroke_7.png'}],new IWSize(168,225))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('TRAVEL_files/TRAVELMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');IMpreload('TRAVEL_files','shapeimage_2','0');IMpreload('TRAVEL_files','shapeimage_2','1');applyEffects()}
