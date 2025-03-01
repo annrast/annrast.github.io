1a34816600d73f50d4e4aa24ee44dc80ddc5cbc4
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:20:15 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/Cissy_Dress_Forms_files/Cissy_Dress_Forms.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Cissy_Dress_Forms_files/Cissy_Dress_FormsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id4');performPostEffectsFixups()}
