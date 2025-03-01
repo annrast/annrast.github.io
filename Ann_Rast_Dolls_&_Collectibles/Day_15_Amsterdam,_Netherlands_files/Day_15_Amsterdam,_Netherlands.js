8a41b1877e5c37ab6f163d76b22705c1d10f9f7e
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:21:44 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/Day_15_Amsterdam,_Netherlands_files/Day_15_Amsterdam,_Netherlands.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Day_15_Amsterdam,_Netherlands_files/Day_15_Amsterdam,_NetherlandsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
