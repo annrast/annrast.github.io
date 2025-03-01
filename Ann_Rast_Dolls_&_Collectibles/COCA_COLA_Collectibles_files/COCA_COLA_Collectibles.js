cdc004e243e4d96839547689798d6eec7a70688d
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:19:58 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/COCA_COLA_Collectibles_files/COCA_COLA_Collectibles.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('COCA_COLA_Collectibles_files/Formal_inset_01.png'),IWCreateImage('COCA_COLA_Collectibles_files/Formal_inset_02.png'),IWCreateImage('COCA_COLA_Collectibles_files/Formal_inset_03.png'),IWCreateImage('COCA_COLA_Collectibles_files/Formal_inset_06.png'),IWCreateImage('COCA_COLA_Collectibles_files/Formal_inset_09.png'),IWCreateImage('COCA_COLA_Collectibles_files/Formal_inset_08.png'),IWCreateImage('COCA_COLA_Collectibles_files/Formal_inset_07.png'),IWCreateImage('COCA_COLA_Collectibles_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('COCA_COLA_Collectibles_files/COCA_COLA_CollectiblesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');fixAllIEPNGs('Media/transparent.gif');IMpreload('COCA_COLA_Collectibles_files','shapeimage_2','0');IMpreload('COCA_COLA_Collectibles_files','shapeimage_2','1');IMpreload('COCA_COLA_Collectibles_files','shapeimage_2','2');IMpreload('COCA_COLA_Collectibles_files','shapeimage_2','3');applyEffects()}
