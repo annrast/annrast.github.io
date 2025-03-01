67946e71bb0a13a8f83b7322b0d9bd4be3e11bf6
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:24:02 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/MADAME_ALEXANDER_DOLLS_files/MADAME_ALEXANDER_DOLLS.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('MADAME_ALEXANDER_DOLLS_files/Formal_inset_01.png'),IWCreateImage('MADAME_ALEXANDER_DOLLS_files/Formal_inset_02.png'),IWCreateImage('MADAME_ALEXANDER_DOLLS_files/Formal_inset_03.png'),IWCreateImage('MADAME_ALEXANDER_DOLLS_files/Formal_inset_06.png'),IWCreateImage('MADAME_ALEXANDER_DOLLS_files/Formal_inset_09.png'),IWCreateImage('MADAME_ALEXANDER_DOLLS_files/Formal_inset_08.png'),IWCreateImage('MADAME_ALEXANDER_DOLLS_files/Formal_inset_07.png'),IWCreateImage('MADAME_ALEXANDER_DOLLS_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('MADAME_ALEXANDER_DOLLS_files/MADAME_ALEXANDER_DOLLSMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
