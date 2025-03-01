36136a78576585fbf0bfc12433d31fa1f8572ed4
Winston Rast <winston+annrast@rast.org>
2025-02-28 20:54:26 -0700

    add a bunch of files dirs

---
+++ Ann_Rast_Dolls_&_Collectibles/__._._._._._._._._._._._1_files/__._._._._._._._._._._._1.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('__._._._._._._._._._._._1_files/__._._._._._._._._._._._1Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
---
+++ Ann_Rast_Dolls_&_Collectibles/__._._._._._._._._._._files/__._._._._._._._._._..js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('__._._._._._._._._._._files/__._._._._._._._._._.Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
