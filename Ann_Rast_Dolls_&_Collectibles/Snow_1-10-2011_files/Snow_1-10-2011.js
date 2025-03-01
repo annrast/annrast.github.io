94fd2b219f869f0222ccc144067c0570703c6c31
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:10:26 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/Snow_1-10-2011_files/Snow_1-10-2011.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,426),url:'Snow_1-10-2011_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Snow_1-10-2011_files/stroke_1.png'},{rect:new IWRect(2,-2,571,4),url:'Snow_1-10-2011_files/stroke_2.png'},{rect:new IWRect(573,-2,5,4),url:'Snow_1-10-2011_files/stroke_3.png'},{rect:new IWRect(573,2,5,426),url:'Snow_1-10-2011_files/stroke_4.png'},{rect:new IWRect(573,428,5,4),url:'Snow_1-10-2011_files/stroke_5.png'},{rect:new IWRect(2,428,571,4),url:'Snow_1-10-2011_files/stroke_6.png'},{rect:new IWRect(-2,428,4,4),url:'Snow_1-10-2011_files/stroke_7.png'}],new IWSize(575,430))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Snow_1-10-2011_files/Snow_1-10-2011Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
