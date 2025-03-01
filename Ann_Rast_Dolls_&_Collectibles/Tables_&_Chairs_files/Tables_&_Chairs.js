e67717a835a485d5dc0f818d7a66963e1b0358c8
Winston Rast <winston+annrast@rast.org>
2025-02-28 21:05:54 -0700

    files

---
+++ Ann_Rast_Dolls_&_Collectibles/Tables_&_Chairs_files/Tables_&_Chairs.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Tables_%26_Chairs_files/Formal_inset_01.png'),IWCreateImage('Tables_%26_Chairs_files/Formal_inset_02.png'),IWCreateImage('Tables_%26_Chairs_files/Formal_inset_03.png'),IWCreateImage('Tables_%26_Chairs_files/Formal_inset_06.png'),IWCreateImage('Tables_%26_Chairs_files/Formal_inset_09.png'),IWCreateImage('Tables_%26_Chairs_files/Formal_inset_08.png'),IWCreateImage('Tables_%26_Chairs_files/Formal_inset_07.png'),IWCreateImage('Tables_%26_Chairs_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Tables_&_Chairs_files/Tables_&_ChairsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id6');applyEffects()}
