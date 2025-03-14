3f70df339d6a5b6ff0a73fe1ef3d214acbb29e0a
Winston Rast <winston+annrast@rast.org>
2025-03-14 15:11:22 -0600

    css png

---
+++ Ann_Rast_Dolls_&_Collectibles/Day_12_Marksburg_Castle,_Page_2_files/Day_12_Marksburg_Castle,_Page_2.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Day_12_Marksburg_Castle,_Page_2_files/Formal_inset_01.png'),IWCreateImage('Day_12_Marksburg_Castle,_Page_2_files/Formal_inset_02.png'),IWCreateImage('Day_12_Marksburg_Castle,_Page_2_files/Formal_inset_03.png'),IWCreateImage('Day_12_Marksburg_Castle,_Page_2_files/Formal_inset_06.png'),IWCreateImage('Day_12_Marksburg_Castle,_Page_2_files/Formal_inset_09.png'),IWCreateImage('Day_12_Marksburg_Castle,_Page_2_files/Formal_inset_08.png'),IWCreateImage('Day_12_Marksburg_Castle,_Page_2_files/Formal_inset_07.png'),IWCreateImage('Day_12_Marksburg_Castle,_Page_2_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Day_12_Marksburg_Castle,_Page_2_files/Day_12_Marksburg_Castle,_Page_2Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
