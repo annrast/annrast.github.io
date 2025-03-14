d717fc6940ba7a61778cf598c1134bbd1182448c
Winston Rast <winston+annrast@rast.org>
2025-03-14 15:04:48 -0600

    js

---
+++ Ann_Rast_Dolls_&_Collectibles/Breckenridge_files/Breckenridge.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20250228
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,296),url:'Breckenridge_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Breckenridge_files/stroke_1.png'},{rect:new IWRect(2,-2,220,4),url:'Breckenridge_files/stroke_2.png'},{rect:new IWRect(222,-2,5,4),url:'Breckenridge_files/stroke_3.png'},{rect:new IWRect(222,2,5,296),url:'Breckenridge_files/stroke_4.png'},{rect:new IWRect(222,298,5,4),url:'Breckenridge_files/stroke_5.png'},{rect:new IWRect(2,298,220,4),url:'Breckenridge_files/stroke_6.png'},{rect:new IWRect(-2,298,4,4),url:'Breckenridge_files/stroke_7.png'}],new IWSize(224,300))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Breckenridge_files/BreckenridgeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
